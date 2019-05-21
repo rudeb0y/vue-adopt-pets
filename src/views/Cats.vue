1947426b7d563ca1593972a7a160d601e1872f27
Stephen Herron <stephen.herron23@gmail.com>
2019-05-21 23:18:00 +0100

    Routing cats to pets/:id

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+    <b-table striped hover :items="cats">
+        <template slot="name" slot-scope="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <router-link :to="`pets/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
