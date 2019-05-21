672ac0876f1cb8b471b40d9169450436234568c6
Stephen Herron <stephen.herron23@gmail.com>
2019-05-21 23:31:43 +0100

    Stubbing out the vuex store

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
      <h1>Dogs for Adoption</h1>
-    <b-table striped hover :items="dogs"></b-table>
+    <b-table striped hover :items="dogs">
+         <template slot="name" slot-scope="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <router-link :to="`pets/dogs/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-import dogs from '@/data/dogs.js'
+import { mapState } from 'vuex'
 
 export default {
   data () {
-    return {
-      dogs
-    }
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
   }
 }
 </script>
